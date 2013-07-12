bc0b452bad93e41a2e92330e29f00ee2714467dd
Kevin Bhayani <kevin.bhayani@gmail.com>
2013-07-12 05:48:48 +0000

    Changed web.js again

--- web.js
+++ web.js
@@ -1,8 +1,7 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
-
-fs.readFileSync("index.html");
 
 app.get('/', function(request, response) {
     var buffer = new Buffer(fs.readFileSync('./index.html'));
